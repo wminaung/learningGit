28371975bf30a2ed039e76e5db34b647087b705e
wminaung <minwin243@gmail.com>
2022-02-04 00:06:10 +0630

    test vs code color bracket

---
+++ app.js
@@ -0,0 +1,15 @@
+let win = true;
+
+function winmin(win) {
+  if (win) {
+    console.log("hello world");
+    win = false;
+    if (win === false) {
+      console.log("inner hello world");
+      win = true;
+    }
+  }
+}
+winmin(win);
+console.log(win);
+console.log("win");
